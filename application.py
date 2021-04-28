9260ba8fc2d9a3cf5acbe940a9be666c68f16e5a
KowaskiAnalytics <sam.vanspauwen1998@gmail.com>
2021-04-28 14:03:42 +0200

    Pathfinder update

---
+++ application.py
@@ -0,0 +1,18 @@
+## Imports
+from flask import Flask, render_template
+from clustercounter.clustercounter_frontend import clustercounter
+
+
+## Browser session
+
+application = Flask(__name__)
+application.register_blueprint(clustercounter, url_prefix= "/clustercounter")
+
+# Homepage
+@application.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', port=8080, debug=True)
