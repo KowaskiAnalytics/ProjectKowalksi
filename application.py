93f76fed619090e9436bb0e447c4b8ebe9b65acf
KowalskiAnalytics <samvanspauwen1998@gmail.com>
2021-04-30 13:28:59 +0200

    test

--- application.py
+++ application.py
@@ -14,5 +14,5 @@ def homepage():
     return render_template("homepage.html")
 
 
-if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=8080, debug=True)
+# if __name__ == "__main__":
+#     application.run(host='0.0.0.0', port=8080, debug=True)
--- server.py
+++
@@ -1,3 +0,0 @@
-from waitress import serve
-import app
-serve(app.app, host='192.168.0.195', port=8081)
